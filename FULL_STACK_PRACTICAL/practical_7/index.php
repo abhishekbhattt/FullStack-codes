58856a2d2bf9c5d37205fd89da95e0dec495716a
Abhishek bhatt <104866090+abhishekbhattt@users.noreply.github.com>
2024-05-28 14:36:40 +0530

    Create a form accepting username and password. Using PHP, connect to a database, and insert the data recieved from the user.

---
+++ FULL_STACK_PRACTICAL/practical_7/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php include '_dbconnect.php'; ?>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        if (empty($username) || empty($password)) {
+            die("Username and password are required.");
+        }
+
+        $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO `user` (`user_pass`, `user_name`) VALUES ('$hashed_pass', '$username');";
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+            die("You are registered successfully");
+        } else {
+            echo "Error: " . mysqli_error($conn);
+        }
+    }
+    ?>
+
+    <form action="index.php" method="post">
+        <label>Username:</label>
+        <input type="text" id="username" name="username">
+        <br><br>
+        <label>Password:</label>
+        <input type="password" id="password" name="password">
+        <br><br>
+        <input type="submit" value="Register">
+    </form>
+</body>
+
+</html>
