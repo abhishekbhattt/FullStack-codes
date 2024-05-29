845b33da7191d96e9bcc82678d60d327da24a234
Abhishek bhatt <104866090+abhishekbhattt@users.noreply.github.com>
2024-05-29 08:16:00 +0530

    Create a sample React application using functional components.

---
+++ FULL_STACK_PRACTICAL/practical_7/src/App.js
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div>
+      <h1>My First React App</h1>
+      <p>
+        This is Graphic Era! We're Glad You Asked! At Graphic Era Hill
+        University, it is a matter of pride that we have three self-contained
+        campuses located at Dehradun, Bhimtal, and Haldwani that offer a
+        comprehensive and enriching academic experience to our students. Our
+        campuses have been meticulously designed to provide world-class
+        amenities, including innovative academic, research, sports, and training
+        facilities, along with luxurious and comfortable student accommodations.
+        All three campuses strive to create a nurturing environment that helps
+        students grow and thrive in all aspects of their lives. Our campuses are
+        home to a diverse student population who come from all over the world,
+        contributing to a vibrant and inclusive learning community.
+      </p>
+    </div>
+  );
+}
+
+export default App;
